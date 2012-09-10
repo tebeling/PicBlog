91945a166c8b6698df49bb47598aa3768ce7163e
Timo Ebeling <t.ebeling@fu-berlin.de>
2012-09-10 18:09:27 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicBlog::Application.config.secret_token = '0ff9df3eb0930506ac1021fb7fc227fd2411d6b914c360b59dc39de34b7da067f7fd4c1b1f105db8f296e3170e7cc5428345bcad9c261efb928c45e5efd42ee8'
