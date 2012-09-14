3b4a5114fb09931f0367cf26e4826960755ec041
Timo Ebeling <t.ebeling@fu-berlin.de>
2012-09-14 20:13:13 +0200

    Add User following and last Correct

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,12 +1,12 @@
 class MicropostsController < ApplicationController
-  before_filter :signed_in_user, only: [:create, :destroy]
-  before_filter :correct_user,   only: :destroy
+  before_filter :signed_in_user
+  before_filter :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      redirect_to root_path
     else
       @feed_items = []
       render 'static_pages/home'
@@ -15,13 +15,16 @@ class MicropostsController < ApplicationController
 
   def destroy
     @micropost.destroy
-    redirect_to root_url
+    redirect_to root_path
   end
 
   private
 
     def correct_user
       @micropost = current_user.microposts.find_by_id(params[:id])
-      redirect_to root_url if @micropost.nil?
+      redirect_to root_path if @micropost.nil?
     end
 end
+
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "PicBlog | Its a Wargamers PicLog"
+    base_title = 'Ruby on Rails Tutorial Sample App'
     if page_title.empty?
       base_title
     else
@@ -10,5 +10,4 @@ module ApplicationHelper
     end
   end
 end
-
 
